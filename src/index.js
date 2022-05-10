515a769e5057abbdd4b0d3b76956d67ba12b6b97
ARANY <v.aderibigbe96@gmail.com>
2022-05-10 09:07:08 -0400

    add markup for landing page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './style.css';
-
- function component() {
-   const element = document.createElement('div');
-
-   element.innerHTML = `<h1>Hello world</h1>`
-
-   return element;
- }
-
- document.body.appendChild(component());
