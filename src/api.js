01409af1619cbea90d92c5a6febcb940e1596ae7
ARANY <v.aderibigbe96@gmail.com>
2022-05-11 09:47:21 -0400

    fix linters

--- src/api.js
+++ src/api.js
@@ -1,23 +1,21 @@
-const gameID = `wegop0c4dTHkWvN72V4E`;
+const gameID = 'wegop0c4dTHkWvN72V4E';
 
-const createScore = (userName, userScore) =>
-  fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify({
-        user: userName,
-        score: userScore,
-      }),
-    }
-  ).then(res => res.json())
+const createScore = (userName, userScore) => fetch(
+  `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+  {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  },
+).then((res) => res.json());
 
-const getScores = () =>
-  fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`
-  ).then(res => res.json())
+const getScores = () => fetch(
+  `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+).then((res) => res.json());
 
-export default {createScore, getScores};
+export default { createScore, getScores };
