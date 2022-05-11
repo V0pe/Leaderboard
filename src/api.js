df34f8ea9a41c12606d5fce30d18ea39f005cbb7
ARANY <v.aderibigbe96@gmail.com>
2022-05-11 09:44:23 -0400

    add the api fetch script

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const gameID = `wegop0c4dTHkWvN72V4E`;
+
+const createScore = (userName, userScore) =>
+  fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        user: userName,
+        score: userScore,
+      }),
+    }
+  ).then(res => res.json())
+
+const getScores = () =>
+  fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`
+  ).then(res => res.json())
+
+export default {createScore, getScores};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './style.css';
+import leaderboard from './api.js';
+
